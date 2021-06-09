be3763afd62755da146b0bd6da666b0c881dad5d
alex <agrau.00@gmail.com>
2021-06-09 11:33:32 +0200

    Extraer Constantes del Main

--- src/examen/TestRPN.java
+++ src/examen/TestRPN.java
@@ -6,13 +6,16 @@ public class TestRPN{
 		
 	
 	
+	private static final String FIN_DE_PROGRAMA = "Fin de programa";
+	private static final String INTRODUCE_EXPRESION_POSTFIJA_O_TECLEA_FIN = "Introduce expresion Postfija o teclea \"fin\".";
+
 	public static void main(String args[]) {
 		while(true) {
 			Scanner in = new Scanner(System.in);
-			System.out.println("Introduce expresion Postfija o teclea \"fin\".");
+			System.out.println(INTRODUCE_EXPRESION_POSTFIJA_O_TECLEA_FIN);
 			String linea = in.nextLine( );
 			if(linea.equals("fin")) {
-				System.out.println("Fin de programa");
+				System.out.println(FIN_DE_PROGRAMA);
 				break;
 			} else {
 				RPN calc = new RPN(linea);
